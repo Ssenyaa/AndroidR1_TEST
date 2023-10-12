d79e203313b2998dd4b28c2d86b7d694d960ac72
Ssenyaa <arseliseev@mail.ru>
2023-10-12 22:45:09 +0300

    коровы и быки не очень поддаются логики

--- Androidproject/app/src/main/java/mai/team5/lab1/MainActivity.java
+++ Androidproject/app/src/main/java/mai/team5/lab1/MainActivity.java
@@ -1,92 +1,125 @@
 package mai.team5.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
-    private TextView randomTextView;
+    private TextView onenum;
+    private TextView resultTextView;
+    private Button checkButton;
+    private int secretNumber;
+    private int attempts;
     Boolean isNew = true;
-    EditText onenum;
-    EditText twonum;
+    private TextView randomTextView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         onenum = findViewById(R.id.onenum);
+        resultTextView = findViewById(R.id.resultTextView);
+        checkButton = findViewById(R.id.checkButton);
 
-        randomTextView = findViewById(R.id.textView);
+        randomTextView = findViewById(R.id.randomTextView);
         int randomNumber = generateRandomNumber(1000, 9999);
         randomTextView.setText(String.valueOf(randomNumber));
 
-    }
+        secretNumber = generateRandomNumber(1000, 9999);
+        attempts = 0;
+        checkButton.setOnClickListener(new View.OnClickListener() {
 
-    private int generateRandomNumber(int min, int max) {
-        Random random = new Random();
-        return random.nextInt((max - min) + 1) + min;
-    }
+            @Override
+            public void onClick(View view) {
+                String guessString = onenum.getText().toString();
+                int guess = Integer.parseInt(guessString);
+                attempts++;
 
+                int bulls = getBulls(secretNumber, guess);
+                int cows = getCows(secretNumber, guess);
+
+                String result = "Popitka #" + attempts + ": " + bulls + " bick, " + cows + " korovi";
+
+                if (bulls == 4) {
+                    result += "\nVi ugadali!";
+                }
+
+                resultTextView.setText(result);
+            }
+        });
+    }
     public void clickNumber(View view) {
         if(isNew)
             onenum.setText("");
         isNew=false;
-        String number1 = onenum.getText().toString();
+        String number = onenum.getText().toString();
         if (view.getId() == R.id.cn0) {
-            number1 = number1 + "0";
+            number = number + "0";
         } else if (view.getId() == R.id.cn1) {
-            number1 = number1 + "1";
+            number = number + "1";
         } else if (view.getId() == R.id.cn2) {
-            number1 = number1 + "2";
+            number = number + "2";
         } else if (view.getId() == R.id.cn3) {
-            number1 = number1 + "3";
+            number = number + "3";
         } else if (view.getId() == R.id.cn4) {
-            number1 = number1 + "4";
+            number = number + "4";
         } else if (view.getId() == R.id.cn5) {
-            number1 = number1 + "5";
+            number = number + "5";
         } else if (view.getId() == R.id.cn6) {
-            number1 = number1 + "6";
+            number = number + "6";
         } else if (view.getId() == R.id.cn7) {
-            number1 = number1 + "7";
+            number = number + "7";
         } else if (view.getId() == R.id.cn8) {
-            number1 = number1 + "8";
+            number = number + "8";
         } else if (view.getId() == R.id.cn9) {
-            number1 = number1 + "9";
+            number = number + "9";
         }
-        onenum.setText(number1);
+        onenum.setText(number);
+    };
+    private int getBulls(int secretNumber, int guess) {
+        int bulls = 0;
 
-        String number2 = twonum.getText().toString();
-        if (view.getId() == R.id.cn0) {
-            number2 = number2 + "0";
-        } else if (view.getId() == R.id.cn1) {
-            number2 = 2 + "1";
-        } else if (view.getId() == R.id.cn2) {
-            number2 = number2 + "2";
-        } else if (view.getId() == R.id.cn3) {
-            number2 = number2 + "3";
-        } else if (view.getId() == R.id.cn4) {
-            number2 = number2 + "4";
-        } else if (view.getId() == R.id.cn5) {
-            number2 = number2 + "5";
-        } else if (view.getId() == R.id.cn6) {
-            number2 = number2 + "6";
-        } else if (view.getId() == R.id.cn7) {
-            number2 = number2 + "7";
-        } else if (view.getId() == R.id.cn8) {
-            number2 = number2 + "8";
-        } else if (view.getId() == R.id.cn9) {
-            number2 = number2 + "9";
+        String secretString = String.valueOf(secretNumber);
+        String guessString = String.valueOf(guess);
+
+        for (int i = 0; i < secretString.length(); i++) {
+            if (secretString.charAt(i) == guessString.charAt(i)) {
+                bulls++;
+            }
         }
-        twonum.setText(number2);
+
+        return bulls;
+    }
+
+    private int getCows(int secretNumber, int guess) {
+        int cows = 0;
+
+        String secretString = String.valueOf(secretNumber);
+        String guessString = String.valueOf(guess);
+
+        for (int i = 0; i < secretString.length(); i++) {
+            if (guessString.contains(String.valueOf(secretString.charAt(i))) &&
+                    secretString.charAt(i) != guessString.charAt(i)) {
+                cows++;
+            }
+        }
+
+        return cows;
+    }
+
+    private int generateRandomNumber(int min, int max) {
+        return (int) (Math.random() * (max - min + 1)) + min;
+    }
+    public void clickObnulenie(View view) {
+        onenum.setText("");
+        isNew = true;
     }
 }
+
+
