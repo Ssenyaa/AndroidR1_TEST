7e894276e863686e0563f5022c8947116cdbf8e9
Ssenyaa <arseliseev@mail.ru>
2023-10-17 00:01:02 +0300

    идеально работает

--- Androidproject/app/src/main/java/mai/team5/lab1/MainActivity.java
+++ Androidproject/app/src/main/java/mai/team5/lab1/MainActivity.java
@@ -16,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
     private int attempts;
     private TextView secretNumberTextView;
 
+    private Button[] button = new Button[4];
+
     private String result = "";
+
+    private String number = "";
 
 
 
@@ -39,61 +43,77 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String guessString = onenum.getText().toString();
                 onenum.setText("");
-                if ((guessString.length() == 4) & (guessString.charAt(0)!=0)){
-                    int guess = Integer.parseInt(guessString);
-                    attempts++;
-                    int bulls = getBulls(secretNumber, guess);
-                    int cows = getCows(secretNumber, guess);
 
-                    result += "Popitka #" + attempts + ": -" + guess + "- " + bulls + " bick, " + cows + " korovi\n";
+                if ((number.length() == 4)) {
+                    if (number.charAt(0) != '0') {
+                        attempts++;
+                        int bulls = getBulls(secretNumber, number);
+                        int cows = getCows(secretNumber, number);
 
-                    if (bulls == 4) {
-                        result += "\nVi ugadali!\n";
+                        result += "Popitka #" + attempts + ": -" + number + "- " + bulls + " bick, " + cows + " korovi\n";
+
+                        if (bulls == 4) {
+                            result += "\nVi ugadali!\n";
+                        }
+                        if (attempts == 5) {
+                            result += "\nVi proigrali!!!\n";
+                        }
+
+                        resultTextView.setText(result);
+
+                        for (int i = 0; i < 4; i++) {
+                            button[i].setEnabled(true);
+                        }
+                    } else {
+                        resultTextView.setText("ЧИСЛО НЕ ДОЛЖНО НАЧИНАТЬСЯ С НУЛЯ");
                     }
-                    if (attempts == 5) {
-                        result += "\nVi proigrali!!!\n";
-                    }
-
-                    resultTextView.setText(result);
                 }
+                else{
+                    resultTextView.setText("ЧИСЛО ДОЛЖНО БЫТЬ ЧЕТЫРЕХЗНАЧНЫМ");
+                }
+
+                number = "";
             }
         });
     }
     public void clickNumber(View view) {
-        String number = onenum.getText().toString();
-        if (view.getId() == R.id.cn0) {
-            number = number + "0";
-        } else if (view.getId() == R.id.cn1) {
-            number = number + "1";
-        } else if (view.getId() == R.id.cn2) {
-            number = number + "2";
-        } else if (view.getId() == R.id.cn3) {
-            number = number + "3";
-        } else if (view.getId() == R.id.cn4) {
-            number = number + "4";
-        } else if (view.getId() == R.id.cn5) {
-            number = number + "5";
-        } else if (view.getId() == R.id.cn6) {
-            number = number + "6";
-        } else if (view.getId() == R.id.cn7) {
-            number = number + "7";
-        } else if (view.getId() == R.id.cn8) {
-            number = number + "8";
-        } else if (view.getId() == R.id.cn9) {
-            number = number + "9";
+        int l = number.length();
+        if (l < 4){
+            if (view.getId() == R.id.cn0) {
+                number = number + "0";
+            } else if (view.getId() == R.id.cn1) {
+                number = number + "1";
+            } else if (view.getId() == R.id.cn2) {
+                number = number + "2";
+            } else if (view.getId() == R.id.cn3) {
+                number = number + "3";
+            } else if (view.getId() == R.id.cn4) {
+                number = number + "4";
+            } else if (view.getId() == R.id.cn5) {
+                number = number + "5";
+            } else if (view.getId() == R.id.cn6) {
+                number = number + "6";
+            } else if (view.getId() == R.id.cn7) {
+                number = number + "7";
+            } else if (view.getId() == R.id.cn8) {
+                number = number + "8";
+            } else if (view.getId() == R.id.cn9) {
+                number = number + "9";
+            }
+            onenum.setText(number);
+            button[l] = findViewById(view.getId());
+            button[l].setEnabled(false);
         }
-        onenum.setText(number);
     };
-    private int getBulls(int secretNumber, int guess) {
+    private int getBulls(int secretNumber, String guess) {
         int bulls = 0;
 
         String secretString = String.valueOf(secretNumber);
-        String guessString = String.valueOf(guess);
+
 
         for (int i = 0; i < secretString.length(); i++) {
-            if (secretString.charAt(i) == guessString.charAt(i)) {
+            if (secretString.charAt(i) == guess.charAt(i)) {
                 bulls++;
             }
         }
@@ -101,15 +121,14 @@ public class MainActivity extends AppCompatActivity {
         return bulls;
     }
 
-    private int getCows(int secretNumber, int guess) {
+    private int getCows(int secretNumber, String guess) {
         int cows = 0;
 
         String secretString = String.valueOf(secretNumber);
-        String guessString = String.valueOf(guess);
 
         for (int i = 0; i < secretString.length(); i++) {
-            if (guessString.contains(String.valueOf(secretString.charAt(i))) &&
-                    secretString.charAt(i) != guessString.charAt(i)) {
+            if (guess.contains(String.valueOf(secretString.charAt(i))) &&
+                    secretString.charAt(i) != guess.charAt(i)) {
                 cows++;
             }
         }
@@ -122,11 +141,14 @@ public class MainActivity extends AppCompatActivity {
         do{
             number = Integer.toString((int) (Math.random() * 10000));
         }while ((number.charAt(0) == number.charAt(1)) | (number.charAt(0) == number.charAt(2)) | (number.charAt(0) == number.charAt(3)) |
-                (number.charAt(1) == number.charAt(2)) | (number.charAt(1) == number.charAt(2)) | (number.charAt(2) == number.charAt(3)));
+                (number.charAt(1) == number.charAt(2)) | (number.charAt(1) == number.charAt(3)) | (number.charAt(2) == number.charAt(3)));
         return Integer.parseInt(number);
     }
     public void clickObnulenie(View view) {
         onenum.setText("");
+        for (int i=0; i<4; i++){
+            button[i].setEnabled(true);
+        }
     }
 }
 
